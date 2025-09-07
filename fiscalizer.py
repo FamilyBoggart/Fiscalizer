409a5b968131a76aa1401d297a3bd39d05774867
Ale <alexanderoga@gmail.com>
2025-09-07 20:44:51 +0200

    Added Interest Count

--- fiscalizer.py
+++ fiscalizer.py
@@ -26,11 +26,20 @@ def create_directory_for_a_file(file):
         os.makedirs(file_name_without_extension)
     return file_name_without_extension
 
+
+#PARSERS
+def parse_USD_equivalent_column(file):
+    """
+    Parsea la columna 'USD Equivalent' de un DataFrame, eliminando símbolos de dólar y comas,
+    y convirtiendo los valores a tipo float."""
+    file["USD Equivalent"] = file["USD Equivalent"].replace(r"[\$,]","", regex=True).str.strip().astype(float)
+    return file
+
 # Lectura del archivo CSV y catalogación de transacciones por tipo
 
 def read_csv_data(ruta):
     """
-    Lee un archivo CSV que contiene datos históricos y devuelve un DataFrame de pandas.
+    Lee un archivo CSV que contiene datos históricos y devuelve un DataFrame de pandas parseado.
 
     Parámetros:
     ruta (str): La ruta al archivo CSV.
@@ -40,6 +49,7 @@ def read_csv_data(ruta):
     """
     try:
         df = pd.read_csv(ruta)
+        df = parse_USD_equivalent_column(df)
         return df
     except Exception as e:
         print(f"Error al leer el archivo {ruta}: {e}")
@@ -92,19 +102,66 @@ def process_transactions(file):
     print(f'Total transactions processed: {total_count}')
 
 
+
+# Calcular intereses recibidos
+def get_coin_names_earned_in_interest(file):
+    """
+    Obtiene una lista de nombres de criptomonedas por las cuales se han recibido intereses en un DataFrame.
+    """
+    return file.loc[file['Type'] == 'Interest','Input Currency'].unique()
+
+def get_interest_by_coin(file, coin):
+    """
+    Calcula el total de intereses recibidos por una criptomoneda específica en un DataFrame.
+    Parámetros:
+    
+    file (pd.DataFrame): El DataFrame
+    coin (str): El nombre de la criptomoneda.
+    Retorna:
+    tuple: Una tupla que contiene la cantidad total de la criptomoneda y su valor en USD.
+    """
+    coin_interest = file.loc[(file['Type'] == 'Interest') & (file['Input Currency'] == coin), ['Input Amount', 'USD Equivalent']]
+    usd_value = coin_interest['USD Equivalent'].sum()
+    coin_amount = coin_interest['Input Amount'].astype(float).sum()
+    return coin_amount, usd_value
+
+def get_total_interest(file):
+    """
+    Calcula el total de intereses recibidos por cada criptomoneda en un DataFrame.
+    """
+    coins = get_coin_names_earned_in_interest(file)
+    total_interest = {}
+    for coin in coins:
+        coin_amount, usd_value = get_interest_by_coin(file, coin)
+        total_interest[coin] = {'amount': coin_amount, 'usd_value': usd_value}
+    return total_interest
+
+def calculate_total_interest(file):
+    total_interest = get_total_interest(file)
+    for coin, data in total_interest.items():
+        coin_amount = data['amount']
+        usd_value = data['usd_value']
+        print(f'Intereses recibidos: {coin_amount} {coin}, valor en USD: {usd_value}')
+    print('-' * 40)
+    print(f'Total intereses en USD: {sum(data["usd_value"] for data in total_interest.values())}')
+
+
 def main():
     """
     Procesa los archivos CSV en el directorio actual, creando un directorio para cada archivo
     y exportando las transacciones por tipo dentro de cada directorio.
     """
     for f in get_csv_files_in_directory():
-        print(f'Processing file: {f}')
+        print(f'Processing file: \033[94m{f}\033[0m')
         print('-' * 40)
 
         dir_name = create_directory_for_a_file(f)
         file  = read_csv_data(f)
         os.chdir(dir_name)
         process_transactions(file)
+        calculate_total_interest(file)
         os.chdir('..')
 
 main()
+
+
